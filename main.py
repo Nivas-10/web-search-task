336e5e6910b11fb4a1acc48f910c65453224a374
Nivas-10 <136707429+Nivas-10@users.noreply.github.com>
2025-06-10 14:56:07 +0530

    fixed errors

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 from collections import defaultdict
 from urllib.parse import urljoin, urlparse
-
 class WebCrawler:
     def __init__(self):
         self.index = defaultdict(list)
